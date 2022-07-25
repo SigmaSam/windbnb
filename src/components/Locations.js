8aea5b1ce6bd251518b88f9e00bbdb23dafdd672
Samuel Almao Herrera <samuelalmaoherrera@gmail.com>
2022-07-24 22:33:52 -0300

    removed unnecesary partials styling.

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,22 +1,8 @@
 import React from 'react';
 
-
 export default function Locations({focused,handleFocus,count,bundle,searchQuery,setSearchQuery}) {
-    /* List of 100 most popular cities */
-
-
-
-    
 
     /*Conditional Styles*/
-    const fStyleInputs = {
-        
-    }
-
-    const fStyleGuest = {
-        
-    }
-
     const fStyleSButton = {
         color: "white",
         background:"#EB5757E5",
@@ -36,7 +22,6 @@ export default function Locations({focused,handleFocus,count,bundle,searchQuery,
         height: "5rem"
     }
     
-   
     return (
         <form className="navbar--search"
                 onSubmit={bundle} 
@@ -44,7 +29,6 @@ export default function Locations({focused,handleFocus,count,bundle,searchQuery,
         >
                 {/*Search box Input with places.*/}
                 <input
-                    style={ !focused ? null : fStyleInputs }
                     onClick={handleFocus}
                     value={searchQuery}
                     onInput={e => setSearchQuery(e.target.value)}
@@ -57,7 +41,6 @@ export default function Locations({focused,handleFocus,count,bundle,searchQuery,
                 {/*Shows how many guests there are.*/}
                 <input 
                     onClick={handleFocus}
-                    style={ !focused ? null : fStyleGuest }
                     defaultValue={
                         (count.adults + count.children === 0)
                         ? "" 
